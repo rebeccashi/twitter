e114c74da2fe040f97d857304d6f041339091a29
Rebecca <rebecca_517@hotmail.com>
2020-10-09 23:47:14 -0400

    login & logout functions

---
+++ Twitter/HomeTableViewController.swift
@@ -0,0 +1,42 @@
+//
+//  HomeTableViewController.swift
+//  Twitter
+//
+//  Created by Rebecca Shi on 10/9/20.
+//  Copyright © 2020 Dan. All rights reserved.
+//
+
+import UIKit
+
+class HomeTableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+
+        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+    }
+    
+    @IBAction func onLogout(_ sender: Any) {
+    TwitterAPICaller.client?.logout()
+        //make this modally presented view disappear
+        self.dismiss(animated: true, completion: nil)
+    }
+    
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 0
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return 0
+    }
+
+
+}
